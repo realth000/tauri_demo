e124786437b3c1808944cdb6de79d1c23021ae99
realth000 <a598919335@gmail.com>
2023-12-07 17:29:39 +0800

    Update code

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,26 +1,28 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/tauri";
+<script lang="ts" setup>
+import {ref} from "vue";
+import {invoke} from "@tauri-apps/api/tauri";
 
 const greetMsg = ref("");
 const name = ref("");
 const myCommandArg = ref("");
 const myCommandResult = ref("");
 
+const clickCount = ref(0);
+
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+  greetMsg.value = await invoke("greet", {name: name.value});
 }
 
 async function myCommand() {
   const time = new Date().toString();
-  myCommandResult.value = await invoke("my_command", {name: myCommandArg.value, time: time}) ;
+  myCommandResult.value = await invoke("my_command", {name: myCommandArg.value, time: time});
 }
 </script>
 
 <template>
   <form class="row" @submit.prevent="greet">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <input id="greet-input" v-model="name" placeholder="Enter a name..."/>
     <button type="submit">Greet</button>
   </form>
 
@@ -29,10 +31,14 @@ async function myCommand() {
   <br>
 
   <form class="row" @submit.prevent="myCommand">
-    <input id="my-command-input" v-model="myCommandArg" placeholder="Run my command..." />
+    <input id="my-command-input" v-model="myCommandArg" placeholder="Run my command..."/>
     <button type="submit">Run My Command</button>
   </form>
 
   <p> {{ myCommandResult }}</p>
+
+  <p>{{ clickCount }}
+    <button @click="clickCount++">Add</button>
+  </p>
 
 </template>
